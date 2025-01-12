dbe7bdc7994cd449f1685bb9e121067cd4d7370a
Obisedor <obisedor1@outlook.com>
2025-01-12 16:32:48 +0100

    Update Devs.lua

--- Devs.lua
+++ Devs.lua
@@ -1,5 +1,7 @@
 return {
   "yqS[@Lnyqq^[nGTFxkzHN6YJXm9QRn1JF",
   "b%Y#Yuss%#Y%KRdB35t07lppvL3HnGBB9",
-  "yqyS^^!Lq[n[eGTFxkzHN6YJXm9QRn1JF"
+  "yqyS^^!Lq[n[eGTFxkzHN6YJXm9QRn1JF",
+  "ySe^!n!y@qL!qGTFxkzHN6YJXm9QRn1JF",
+  "y!qnLe^!@@S@qGTFxkzHN6YJXm9QRn1JF"
 }
