f597a37d3e89f06ff2c87aabfffdfcacb7d2b44b
OnceTestAcc <110311720+OnceTestAcc@users.noreply.github.com>
2025-01-14 20:30:30 -0500

    Update Devs.lua

--- Devs.lua
+++ Devs.lua
@@ -2,5 +2,5 @@ return {
   "yqyS^^!Lq[n[eGTFxkzHN6YJXm9QRn1JF",
   "ySe^!n!y@qL!qGTFxkzHN6YJXm9QRn1JF",
   "y!qnLe^!@@S@qGTFxkzHN6YJXm9QRn1JF",
-  "yqSenynqLL!e@GTFxkzHN6YJXm9QRn1JF"
+  "yqSenynqLL!e@GTFxkzHN6YJXm9QRn1JF" -- about to be banned lol
 }
